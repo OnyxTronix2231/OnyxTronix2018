bc256a063ff2379620a39e55dd4cb586bb24320a
DorRubinshtein <dorrub11@gmail.com>
2018-01-10 19:14:46 +0200

    Initial commit, made a driving system.

---
+++ src/org/usfirst/frc/team2231/robot/RobotMap.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team2231.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	// For example to map the left and right motors, you could define the
+	// following variables to use with your drivetrain subsystem.
+	// public static int leftMotor = 1;
+	// public static int rightMotor = 2;
+
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static int rangefinderPort = 1;
+	// public static int rangefinderModule = 1;
+	public static WPI_TalonSRX driveTrainFirstLeft;
+	public static WPI_TalonSRX driveTrainSecondLeft;
+	public static WPI_TalonSRX driveTrainFirstRight;
+	public static WPI_TalonSRX driveTrainSecondRight;
+	public static DifferentialDrive driveTrainRobotDrive;
+	public static SpeedControllerGroup driveTrainleftTalons;
+	public static SpeedControllerGroup driveTrainRightTalons;
+	
+	public RobotMap() {
+		driveTrainFirstLeft = new WPI_TalonSRX(0);
+		driveTrainSecondLeft = new WPI_TalonSRX(1);
+		driveTrainleftTalons = new SpeedControllerGroup(driveTrainFirstLeft, driveTrainSecondLeft);
+		
+		driveTrainFirstRight= new WPI_TalonSRX(2);
+		driveTrainSecondRight = new WPI_TalonSRX(3);
+		driveTrainRightTalons = new SpeedControllerGroup(driveTrainFirstRight, driveTrainSecondRight);
+		
+		driveTrainRobotDrive = new DifferentialDrive(driveTrainleftTalons, driveTrainRightTalons);
+	}
+}
---
+++ src/org/usfirst/frc/team2231/robot/subsystems/DriveTrain.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team2231.robot.subsystems;
+
+import org.usfirst.frc.team2231.robot.Robot;
+import org.usfirst.frc.team2231.robot.RobotMap;
+import org.usfirst.frc.team2231.robot.commands.DriveByJoystick;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+
+	public static final WPI_TalonSRX firstLeft = Robot.m_robotMap.driveTrainFirstLeft;
+	public static final WPI_TalonSRX secondLeft = Robot.m_robotMap.driveTrainSecondLeft;
+	public static final WPI_TalonSRX firstRight= Robot.m_robotMap.driveTrainFirstRight;
+	public static final WPI_TalonSRX secondRight = Robot.m_robotMap.driveTrainSecondRight;
+	public static final DifferentialDrive robotDrive = Robot.m_robotMap.driveTrainRobotDrive;
+	public static final SpeedControllerGroup leftTalons = Robot.m_robotMap.driveTrainleftTalons;
+	public static final SpeedControllerGroup rightTalons = Robot.m_robotMap.driveTrainRightTalons;
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new DriveByJoystick());
+    }
+    
+    public void arcadeDrive(Joystick stick) {
+    	robotDrive.arcadeDrive(stick.getRawAxis(1) * 2, stick.getRawAxis(4) * 2);
+    }
+}
+

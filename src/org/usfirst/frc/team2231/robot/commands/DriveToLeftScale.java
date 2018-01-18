4d1d6530b5c6256608fe3901edf402c92b6062c9
Leanne08 <leanne.buchnik@gmail.com>
2018-01-19 01:43:51 +0200

    the commits by tal mashiach

--- src/org/usfirst/frc/team2231/robot/commands/DriveToLeftScale.java
+++ src/org/usfirst/frc/team2231/robot/commands/DriveToLeftScale.java
@@ -27,10 +27,10 @@ public class DriveToLeftScale extends CommandGroup {
     	
     	//addSequential(new ElevatorPitchDrop());
     	addSequential(new Collect());
-    	addSequential(new DriveByDistance(193,193));
+    	addSequential(new DriveByDistance(193));
     	//addSequential(new ElevatorPitchRaise());
     	addSequential(new DriveByDistance(86.5, 58.5));
-    	addSequential(new DriveByDistance(133.47, 133.47));
+    	addSequential(new DriveByDistance(133.47));
     	addSequential(new DriveByDistance(70,98));
     	addSequential(new DriveByDistance(-30.6, 30.6));
     	addSequential(new Raise());

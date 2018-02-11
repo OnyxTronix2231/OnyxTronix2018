6237e6607964f9183680d14050581ca1895a81e2
Leanne08 <leanne.buchnik@gmail.com>
2018-02-11 23:31:45 +0200

    Created a class FieldPositions

---
+++ src/org/usfirst/frc/team2231/robot/FieldPositions.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team2231.robot;
+
+public class FieldPositions {
+
+	private Position m_closeSwitch;
+	private Position m_scale;
+	private Position m_distantSwitch;
+	
+	public FieldPositions(Position m_closeSwitch, Position m_scale, Position m_distantSwitch) {
+		this.m_closeSwitch = m_closeSwitch;
+		this.m_scale = m_scale;
+		this.m_distantSwitch = m_distantSwitch;
+	}	
+}
---
+++ src/org/usfirst/frc/team2231/robot/Position.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team2231.robot;
+
+public enum Position {
+	Left, Right
+}

65160fa9f05003db6e2c78d5b4619e815ddcdfda
Leanne08 <leanne.buchnik@gmail.com>
2018-02-11 23:32:45 +0200

    more things

--- src/org/usfirst/frc/team2231/robot/FieldPositions.java
+++ src/org/usfirst/frc/team2231/robot/FieldPositions.java
@@ -6,9 +6,9 @@ public class FieldPositions {
 	private Position m_scale;
 	private Position m_distantSwitch;
 	
-	public FieldPositions(Position m_closeSwitch, Position m_scale, Position m_distantSwitch) {
-		this.m_closeSwitch = m_closeSwitch;
-		this.m_scale = m_scale;
-		this.m_distantSwitch = m_distantSwitch;
+	public FieldPositions(Position closeSwitch, Position scale, Position distantSwitch) {
+		this.m_closeSwitch = closeSwitch;
+		this.m_scale = scale;
+		this.m_distantSwitch = distantSwitch;
 	}	
 }

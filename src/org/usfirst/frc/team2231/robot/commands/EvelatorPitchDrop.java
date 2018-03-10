ac8e3b32cc0512b7a1bde1bbeb0f712853e491c4
nadavziri <nadavziri123@gmail.com>
2018-03-10 22:31:58 +0200

    Changed the elevator pitch Commands

--- src/org/usfirst/frc/team2231/robot/commands/EvelatorPitchDrop.java
+++ src/org/usfirst/frc/team2231/robot/commands/EvelatorPitchDrop.java
@@ -1,29 +1,33 @@
 package org.usfirst.frc.team2231.robot.commands;
 
 import org.usfirst.frc.team2231.robot.Robot;
-import edu.wpi.first.wpilibj.command.TimedCommand;
 
+import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class EvelatorPitchDrop extends TimedCommand {
+public class EvelatorPitchDrop extends Command {
 	private static final double SPEED = 0.4;
 
     public EvelatorPitchDrop() {
-		super(0.5);
+    	requires(Robot.m_elevatorPitch);
 	}
-
-
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.m_elevatorPitch.setSpeed(SPEED);    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.m_elevatorPitch.setSpeed(SPEED);
     }
 
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
     // Called once after isFinished returns true
     protected void end() {
     	Robot.m_elevatorPitch.stop();

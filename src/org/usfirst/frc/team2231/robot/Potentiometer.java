aea3a64bb91eb21de95089b7f87d9656693b2047
nadavziri <nadavziri123@gmail.com>
2018-03-11 23:17:17 +0200

    Adapted Potentiometer into PIDSource.

---
+++ src/org/usfirst/frc/team2231/robot/Potentiometer.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team2231.robot;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+public class Potentiometer implements PIDSource{
+
+	public AnalogInput m_potentiometer;
+	public double m_minimumPotentionmeter;
+	public double m_maximumPotentionmeter;
+	public double m_minimumHeight;
+	public double m_maximumHeight;
+	
+	public Potentiometer(AnalogInput potentiometer, double minimumPotentiometer, double maximumPotentiometer, double minimumHeight,
+			double maximumHeight) {
+		m_potentiometer = potentiometer;
+		m_minimumPotentionmeter = minimumPotentiometer;
+		m_maximumPotentionmeter = maximumPotentiometer;
+		m_minimumHeight = minimumHeight;
+		m_maximumHeight = maximumHeight;
+	}
+	
+	public double mapRange(double firstMinimalValue, double firstMaximalValue, double secondMinimalValue,
+			double secondMaximalValue, double value) {
+		return secondMinimalValue + ((value - firstMinimalValue) * (secondMaximalValue - secondMinimalValue))
+				/ (firstMaximalValue - firstMinimalValue);
+	}
+	
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+		m_potentiometer.setPIDSourceType(pidSource);
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		return m_potentiometer.getPIDSourceType();
+	}
+	
+	public double getHeightFromVoltage(double voltage) {
+		return mapRange(m_minimumPotentionmeter, m_maximumPotentionmeter, m_minimumHeight, m_maximumHeight, voltage);
+	}
+
+	@Override
+	public double pidGet() {
+		return getHeightFromVoltage(m_potentiometer.getVoltage());
+	}
+}
